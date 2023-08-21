d617c1b92ed1f1b667a2535091a23ae5628f32bb
Lucas-nmp <lucas.morandeira.parejo@gmail.com>
2023-08-21 11:15:15 +0200

    Cambio el  return a la funcion de buscar ciudad

--- funciones.py
+++ funciones.py
@@ -1,4 +1,7 @@
 from geopy.geocoders import Nominatim
+import bs4
+import requests
+
 
 
 def buscarCiudad(ciudad):
@@ -11,6 +14,8 @@ def buscarCiudad(ciudad):
     datos = getLoc.raw
     datosConcretos = datos['display_name']
     listaDatos = datosConcretos.split(', ')
-    return (f'La localización buscada es {listaDatos[0]}, de {listaDatos[1]}, con el código postal {listaDatos[-2]}, {listaDatos[-1]}?')
-    
+    print(listaDatos)
+    #print(f'La localización buscada es {listaDatos[0]}, de {listaDatos[1]}, con el código postal {listaDatos[-2]}, {listaDatos[-1]}?')
+    # return (f'La localización buscada es {listaDatos[0]}, de {listaDatos[1]}, con el código postal {listaDatos[-2]}, {listaDatos[-1]}?')
+    return listaDatos[0], listaDatos[-1]
 
--- informacionLocal.py
+++ informacionLocal.py
@@ -1,8 +1,16 @@
 import funciones
 
 try:
-    ciudad = input("Escriba el nombre de la población o ciudad de la que desee consultar la hora de amanecer y atardecer:")
-    print(funciones.buscarCiudad(ciudad))
+    respuesta = ''
+    intento = 0
+    while respuesta != 'si':
+        if (intento > 0):
+            print('Escriba algún dato más de la población para una mejor busqueda')
+        ciudad = input("Escriba el nombre de la población o ciudad de la que desee consultar la hora de amanecer y atardecer:")
+        poblacion, pais = funciones.buscarCiudad(ciudad)
+        respuesta = input('Es correcta la población buscada? (si o no)').lower
+        intento += 1
+        
 
 except ValueError:
     print('la ciudad introducida no da resultados')
